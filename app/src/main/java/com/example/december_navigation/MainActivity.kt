b9d097c1d78f550b9be0f830973d78d0fee57344
AlexGrig91 <83690042+AlexGrig91@users.noreply.github.com>
2024-12-21 14:14:33 +0530

    Удалил не нужные файлы

--- app/src/main/java/com/example/december_navigation/MainActivity.kt
+++ app/src/main/java/com/example/december_navigation/MainActivity.kt
@@ -18,30 +18,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            December_NavigationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    December_NavigationTheme {
-        Greeting("Android")
     }
 }
